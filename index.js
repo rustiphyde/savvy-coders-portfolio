97e1f5116af7ce8d0b6f0691bbe8916b23d81777
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-01 20:34:10 -0500

    Add first and last name prompts

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-
+      let nameArr =[];
       //create 'prompt' to ask for name;
       //'name' is here as a GLOBAL VARIABLE that 'anyone' in the <script> can freely access and modify;
-      let name = prompt("Hello! What is your name?");
+      nameArr[0] = prompt("Hello! What is your first name?");
+      nameArr[1] = prompt("Hello! What is your last name?");
+//push add elements to the end of an Array
+      // nameArr.push(fname);
+      // nameArr.push(lname);
       //this is a FUNCTION DECLARATION;
+
+      //userName is an Array of 'first name' and 'last name';
       function checkName(userName) {
         //Check if name is a blank string;
-        if (!userName) {
-            //If the name is blank, ask again and be sure to capture/overwrite the value of userName...runs if checkName is false-y;
-          userName = prompt(
-            "Are you there? What is your name?"
+        if (!userName[0] || !userName[1]) {
+            //If either first or last name is blank, ask again and be sure to capture/overwrite the value of userFirstName and/or userLastName ...runs if checkName is false-y;
+            //Developer's note - We are requiring both pieces of information even if only one is blank;
+
+
+          userName[0] = prompt(
+            "Are you there? What is your first name?"
+          );
+          userName[1] = prompt(
+            "Are you there? What is your last name?"
           );
 
           //RECURSION - fxn calls itself again;
@@ -22,6 +34,6 @@
       // let checkName = function() {};
 
       //We capture what is RETURNED by checkName and overwrite 'name' with that value;
-      name = checkName(name);
+      nameArr = checkName(nameArr);
       // Use 'alert' to say hello to the user, using the name they passed in;
-      document.querySelector("h1").textContent = `Hello, ${name}`;
+      document.querySelector("#greeting").innerHTML = `<p>Hello, ${nameArr[0]} ${nameArr[1]};</p>`;
