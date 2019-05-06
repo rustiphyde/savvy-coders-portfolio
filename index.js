25c32819db35767e29d2504ff66bc2cb0af9c552
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-06 18:38:04 -0500

    Add root div container

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-import Navigation from "./components/Navigation";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
-//Use innerHTML property as a GETTER;
-let initialHTML = document.body.innerHTML;
-
-//Uses innerHTML property as a SETTER;
-document.body.innerHTML = `
+// Uses innerHTML property as a SETTER;
+document.querySelector('#root').innerHTML = `
 ${Navigation}
 ${Header}
 ${Content}
-${Footer}
-${initialHTML}`;
+${Footer}`;
