b7358d57af0b7cf3c7a0fc1d9288d99c6439852e
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-07 13:37:37 -0500

    Add Mobile link to Header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 export default function Header(state){
     return `<header class="container">
 <h1>${state.title}</h1>
+<p><a href="tel:3143373887"><i class="fas fa-phone-square"></i></a> <--Call my cell!!</p>
 <div id="greeting"></div>
 </header>`;
 }
