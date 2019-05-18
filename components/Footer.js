497e8b09fc430a19eee630657421b4058ed6056f
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-18 00:38:58 -0500

    Add alt tags for link icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,25 @@
-export default () => `
+export default () => {
+  return `
 <footer class="container">
-<a href="https://www.deviantart.com/rustiphyde" target="_blank"
+<a href="https://www.deviantart.com/rustiphyde" alt="link to rustiphyde's profile on deviantart" target="_blank"
   ><i class="fab fa-deviantart"></i
 ></a>
-<a href="https://soundcloud.com/rustiphydebeats" target="_blank"
+<a href="https://soundcloud.com/rustiphydebeats" alt="link to rustiphyde beats on soundcloud" target="_blank"
   ><i class="fab fa-soundcloud"></i
 ></a>
-<a href="https://instagram.com/rustiphyde" target="_blank"
+<a href="https://instagram.com/rustiphyde" alt="link to rustiphyde's profile on instagram" target="_blank"
   ><i class="fab fa-instagram"></i
 ></a>
-<a href="https://twitter.com/Rustiphyde" target="_blank"
+<a href="https://twitter.com/Rustiphyde" alt="link to rustiphyde's profile on twitter" target="_blank"
   ><i class="fab fa-twitter"></i
 ></a>
-<a href="https://facebook.com/RustyHoppins" target="_blank"
+<a href="https://facebook.com/RustyHoppins" alt="link to Rusty Hoppins's profile on facebook" target="_blank"
   ><i class="fab fa-facebook-square"></i
 ></a>
-<a href="https://github.com/rustiphyde" target="_blank"
+<a href="https://github.com/rustiphyde" alt="link to rustiphyde's profile on github" target="_blank"
   ><i class="fab fa-github"></i
 ></a>
 <br /><br />
 <strong>&copy; 2019 Rusty Hoppins</strong>
 </footer>`;
+};
