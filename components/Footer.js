a3561e5e2bf47f90b0b0431550f0d86cb8efdc6b
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-20 13:26:41 -0500

    Add accessibilty to link icons in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,23 @@
 export default () => {
   return `
 <footer class="container">
-<a href="https://www.deviantart.com/rustiphyde" alt="link to rustiphyde's profile on deviantart" target="_blank"
-  ><i class="fab fa-deviantart"></i
+<a href="https://www.deviantart.com/rustiphyde" target="_blank" aria-label="Open deviant art profile for Rustiphyde in a new window"
+  ><i aria-hidden="true" class="fab fa-deviantart"></i
 ></a>
-<a href="https://soundcloud.com/rustiphydebeats" alt="link to rustiphyde beats on soundcloud" target="_blank"
-  ><i class="fab fa-soundcloud"></i
+<a href="https://soundcloud.com/rustiphydebeats" target="_blank" aria-label="Open soundcloud page for Rustiphyde Beats in a new window"
+  ><i aria-hidden="true" class="fab fa-soundcloud"></i
 ></a>
-<a href="https://instagram.com/rustiphyde" alt="link to rustiphyde's profile on instagram" target="_blank"
-  ><i class="fab fa-instagram"></i
+<a href="https://instagram.com/rustiphyde" target="_blank" aria-label="Open instagram profile for Rustiphyde in a new window"
+  ><i aria-hidden="true" class="fab fa-instagram"></i
 ></a>
-<a href="https://twitter.com/Rustiphyde" alt="link to rustiphyde's profile on twitter" target="_blank"
-  ><i class="fab fa-twitter"></i
+<a href="https://twitter.com/Rustiphyde" target="_blank" aria-label="Open twitter account for Rustiphyde in a new window"
+  ><i aria-hidden="true" class="fab fa-twitter"></i
 ></a>
-<a href="https://facebook.com/RustyHoppins" alt="link to Rusty Hoppins's profile on facebook" target="_blank"
-  ><i class="fab fa-facebook-square"></i
+<a href="https://facebook.com/RustyHoppins" target="_blank" aria-label="Open facebook profile for Rusty Hoppins in a new window"
+  ><i aria-hidden="true" class="fab fa-facebook-square"></i
 ></a>
-<a href="https://github.com/rustiphyde" alt="link to rustiphyde's profile on github" target="_blank"
-  ><i class="fab fa-github"></i
+<a href="https://github.com/rustiphyde" target="_blank" aria-label="Open github account for rustiphyde in a new window"
+  ><i aria-hidden="true" class="fab fa-github"></i
 ></a>
 <br /><br />
 <strong>&copy; 2019 Rusty Hoppins</strong>
