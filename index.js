10057ba56d9b0e8b7c4c61c00e64379b75e91abd
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-29 23:05:55 -0500

    Comment out axios til the code is completed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import Footer from './components/Footer';
 import * as states from './store';
 
 import Navigo from 'navigo';
-import Axios from 'axios';
+// import Axios from 'axios';
 
 // Object Destructuring uses braces to pull one thing from an object
 import { capitalize } from 'lodash';
